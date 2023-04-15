a2f6e5c67b39ced121f29b333f92b941e5ad3917
otherfutures <109718981+otherfutures@users.noreply.github.com>
2023-04-15 19:43:43 +0900

    Update move_files.py

--- src/move_files.py
+++ src/move_files.py
@@ -3,7 +3,7 @@ import shutil
 
 
 def move_files(path, music, videos, pictures, documents, applications, archives):
-
+    
     music_ext = [
         ".mp3",
         ".m4a",
@@ -11,7 +11,8 @@ def move_files(path, music, videos, pictures, documents, applications, archives)
         ".wma",
         ".aac",
         ".flac",
-        ".alac"
+        ".alac",
+        ".ogg"
     ]
 
     videos_ext = [
@@ -20,6 +21,8 @@ def move_files(path, music, videos, pictures, documents, applications, archives)
         ".webm",
         ".mov",
         ".mpeg",
+        ".mpg",
+        ".vob",
         ".avi",
         ".mkv",
         ".wmv",
@@ -33,6 +36,7 @@ def move_files(path, music, videos, pictures, documents, applications, archives)
         ".jpeg",
         ".gif",
         ".tiff",
+        ".psd",
         ".raw",
         ".svg",
         ".webp",
@@ -46,46 +50,102 @@ def move_files(path, music, videos, pictures, documents, applications, archives)
         ".avif"
     ]
 
+    programs_ext = [
+        ".py", 
+        ".java", 
+        ".c", 
+        ".cpp", 
+        ".js", 
+        ".rb", 
+        ".php", 
+        ".hs", 
+        ".ml", 
+        ".mli", 
+        ".pl", 
+        ".go",
+        ".css", 
+        ".xml", 
+        ".json", 
+        ".csv"
+        ".sql", 
+        ".class", 
+        ".jar", 
+        ".dll"
+    ]
+
     documents_ext = [
         ".pdf",
-        ".epub",
         ".doc",
         ".docx",
-        ".html",
-        ".htm",
         ".xls",
         ".xlsx",
         ".txt",
-        ".csv",
-        ".mobi"
+        ".ppt",
+        ".pptx",
+        ".html",
+        ".htm",
+        ".rtf", 
+        ".odt"
     ]
 
     applications_ext = [
         ".exe",
         ".apk",
         ".deb",
-        ".app"
+        ".app",
+        ".msi",
+        ".dmg",
+        ".rpm"
     ]
 
     archives_ext = [
-        ".zip"
+        ".zip",
+        ".rar",
+        ".7z",
+        ".tar",
+        ".gz",
+        ".bz2"
+        
+    ]
+
+    subtitles_ext = [
+        ".srt", 
+        ".sub", 
+        ".ass", 
+        ".vtt", 
+        ".stl"
+    ]
+
+    ebooks_ext = [
+        ".epub",
+        ".mobi",
+        ".djv",
+        ".azw", 
+        ".azw3", 
+        ".prc"
     ]
 
     files = os.listdir(path)
 
     for file in files:
         filepath = f"{path}/{file}"
-        ext = os.path.splitext(filepath)[1]
+        ext = os.path.splitext(filepath)[1].lower()
         if ext in music_ext:
             shutil.move(filepath, music)
         elif ext in videos_ext:
-            shutil.move(filepath, videos)
+            shutil.move(filepath, loose_videos)
         elif ext in pictures_ext:
             shutil.move(filepath, pictures)
+        elif ext in programs_ext:
+            shutil.move(filepath, programs)
         elif ext in documents_ext:
             shutil.move(filepath, documents)
         elif ext in applications_ext:
             shutil.move(filepath, applications)
         elif ext in archives_ext:
             shutil.move(filepath, archives)
+        elif ext in subtitles_ext:
+            shutil.move(filepath, subtitles)
+        elif ext in ebooks_ext:
+            shutil.move(filepath, ebooks)
     return 0
