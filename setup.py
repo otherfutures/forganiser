49b2f26bd9cb38c32dec96efbef81006dbcc0d41
otherfutures <109718981+otherfutures@users.noreply.github.com>
2023-08-08 07:45:06 +0900

    Update setup.py (v1.4)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="forganiser",
-    version="1.2",
+    version="1.4",
     author="otherfutures, Jigyasu",
     description="A nifty tool to sort your messy folders",
     long_description=long_description,
